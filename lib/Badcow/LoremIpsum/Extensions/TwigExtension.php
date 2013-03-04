81d3f806ded180cc3b93f1c728f33ca4fcd96004
Sam Williams <samuel.williams8@gmail.com>
2013-03-04 15:26:01 +1100

    Added twig extension.

---
+++ lib/Badcow/LoremIpsum/Extensions/TwigExtension.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of the Badcow Lorem Ipsum Generator.
+ *
+ * (c) Samuel Williams <sam@badcow.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Badcow\LoremIpsum\Extensions;
+
+class TwigExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('lorem_ipsum', array($this, 'getLoremIpsum')),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'lorem_ipsum';
+    }
+
+    /**
+     * @param int $count
+     * @param string $format
+     * @param bool $loremipsum
+     * @return array|string
+     */
+    public function getLoremIpsum($count = 300, $format = 'html', $loremipsum = true)
+    {
+        $generator = new \Badcow\LoremIpsum\Generator;
+
+        return $generator->getContent($count, $format, $loremipsum);
+    }
+}
