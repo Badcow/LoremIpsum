89ca442188ac359171e4e20ea33442adabd0b173
Sam Williams <samuel.williams8@gmail.com>
2013-03-05 18:35:17 +1100

    Major refactoring.

--- lib/Badcow/LoremIpsum/Generator.php
+++ lib/Badcow/LoremIpsum/Generator.php
@@ -14,9 +14,37 @@ namespace Badcow\LoremIpsum;
 class Generator
 {
     /**
+     * The mean word length of a sentence
+     *
+     * @var float
+     */
+    protected $sentenceMean = 24.460;
+
+    /**
+     * The standard deviation of words in a sentence
+     *
+     * @var float
+     */
+    protected $sentenceStDev = 5.080;
+
+    /**
+     * Mean number of sentences per paragraph
+     *
+     * @var float
+     */
+    protected $paragraphMean = 5.800;
+
+    /**
+     * The standard deviation of sentences in a paragraph
+     *
+     * @var float
+     */
+    protected $paragraphStDev = 1.930;
+
+    /**
      * @var array
      */
-    private $words = array(
+    protected $words = array(
         'lorem',        'ipsum',        'dolor',        'sit',          'amet',         'consectetur',
         'adipiscing',   'elit',         'curabitur',    'vel',          'hendrerit',    'libero',
         'eleifend',     'blandit',      'nunc',         'ornare',       'odio',         'ut',
@@ -50,272 +78,209 @@ class Generator
     );
 
     /**
-     * @var int
+     * @param $words
+     * @return Generator
      */
-    private $wordsPerParagraph = 100;
+    public function setWords(array $words)
+    {
+        $this->words = $words;
+
+        return $this;
+    }
 
     /**
-     * @var float
+     * Add a single or multiple words to the
+     * generator
+     *
+     * @param string|array $words
      */
-    private $wordsPerSentence = 24.460;
+    public function addWords($words)
+    {
+        if (is_array($words)) {
+            $this->words = array_merge($this->words, $words);
+
+            return;
+        }
+
+        $this->words[] = $word;
+    }
 
     /**
-     * @param null|int $wordsPerParagraph
-     * @param null|float $wordPerSentence
+     * @return array
      */
-    public function __construct($wordsPerParagraph = null, $wordPerSentence = null)
+    public function getWords()
+    {
+        return $this->words;
+    }
+
+    /**
+     * @param float $paragraphMean
+     */
+    public function setParagraphMean($paragraphMean)
+    {
+        $this->paragraphMean = $paragraphMean;
+    }
+
+    /**
+     * @return float
+     */
+    public function getParagraphMean()
+    {
+        return $this->paragraphMean;
+    }
+
+    /**
+     * @param float $paragraphStDev
+     */
+    public function setParagraphStDev($paragraphStDev)
+    {
+        $this->paragraphStDev = $paragraphStDev;
+    }
+
+    /**
+     * @return float
+     */
+    public function getParagraphStDev()
+    {
+        return $this->paragraphStDev;
+    }
+
+    /**
+     * @param float $sentenceMean
+     */
+    public function setSentenceMean($sentenceMean)
+    {
+        $this->sentenceMean = $sentenceMean;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSentenceMean()
+    {
+        return $this->sentenceMean;
+    }
+
+    /**
+     * @param float $sentenceStDev
+     */
+    public function setSentenceStDev($sentenceStDev)
+    {
+        $this->sentenceStDev = $sentenceStDev;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSentenceStDev()
+    {
+        return $this->sentenceStDev;
+    }
+
+    /**
+     * Get an array of random words from $words
+     *
+     * @param int $count
+     * @return array
+     */
+    public function getRandomWords($count)
 	{
-		if (null !== $wordsPerParagraph) {
-            $this->wordsPerParagraph = $wordsPerParagraph;
+        $words = array();
+
+        for ($i = 0; $i < $count; $i++) {
+            $word = $this->words[array_rand($this->words)];
+            if ($i > 0 && $words[$i - 1] === $word) {
+                $i--;
+                continue;
+            }
+
+            $words[] = $word;
         }
 
-        if (null !== $wordPerSentence) {
-            $this->wordsPerSentence = $wordPerSentence;
+		return $words;
+	}
+
+    /**
+     * Get an array of sentences
+     *
+     * @param int $count
+     * @return array
+     */
+    public function getSentences($count)
+    {
+        $sentences = array();
+
+        for ($i = 0; $i < $count; $i++) {
+            $wordCount = Statistics::gauss_ms($this->sentenceMean, $this->sentenceStDev);
+            $sentence = $this->getRandomWords($wordCount);
+            $sentences[] = $this->toSentence($sentence);
         }
-	}
 
-	public function getContent($count, $format = 'html', $loremipsum = true)
-	{
-		$format = strtolower($format);
+        return $sentences;
+    }
 
-		if($count <= 0)
-			return '';
+    /**
+     * Get an array of paragraphs
+     *
+     * @param $count
+     * @return array
+     */
+    public function getParagraphs($count)
+    {
+        $paragraphs = array();
 
-		switch($format)
-		{
-			case 'txt':
-				return $this->getText($count, $loremipsum);
-			case 'plain':
-				return $this->getPlain($count, $loremipsum);
-			default:
-				return $this->getHTML($count, $loremipsum);
-		}
-	}
+        for ($i = 0; $i < $count; $i++) {
+            $number = Statistics::gauss_ms($this->paragraphMean, $this->paragraphStDev);
+            $sentences = $this->getSentences($number);
+            $paragraphs[] = implode(' ', $sentences);
+        }
 
-	private function getWords(&$arr, $count, $loremipsum)
-	{
-		$i = 0;
-		if($loremipsum)
-		{
-			$i = 2;
-			$arr[0] = 'lorem';
-			$arr[1] = 'ipsum';
-		}
+        return $paragraphs;
+    }
 
-		for($i; $i < $count; $i++)
-		{
-			$index = array_rand($this->words);
-			$word = $this->words[$index];
-			//echo $index . '=>' . $word . '<br />';
-
-			if($i > 0 && $arr[$i - 1] == $word)
-				$i--;
-			else
-				$arr[$i] = $word;
-		}
-	}
-
-	private function getPlain($count, $loremipsum, $returnStr = true)
-	{
-		$words = array();
-		$this->getWords($words, $count, $loremipsum);
-		//print_r($words);
-
-		$delta = $count;
-		$curr = 0;
-		$sentences = array();
-		while($delta > 0)
-		{
-			$senSize = $this->gaussianSentence();
-			//echo $curr . '<br />';
-			if(($delta - $senSize) < 4)
-				$senSize = $delta;
-
-			$delta -= $senSize;
-
-			$sentence = array();
-			for($i = $curr; $i < ($curr + $senSize); $i++)
-				$sentence[] = $words[$i];
-
-			$this->punctuate($sentence);
-			$curr = $curr + $senSize;
-			$sentences[] = $sentence;
-		}
-
-		if($returnStr)
-		{
-			$output = '';
-			foreach($sentences as $s)
-				foreach($s as $w)
-					$output .= $w . ' ';
-
-			return $output;
-		}
-		else
-			return $sentences;
-	}
-
-	private function getText($count, $loremipsum)
-	{
-		$sentences = $this->getPlain($count, $loremipsum, false);
-		$paragraphs = $this->getParagraphArr($sentences);
-
-		$paragraphStr = array();
-		foreach($paragraphs as $p)
-		{
-			$paragraphStr[] = $this->paragraphToString($p);
-		}
-
-		$paragraphStr[0] = "\t" . $paragraphStr[0];
-		return implode("\n\n\t", $paragraphStr);
-	}
-
-	private function getParagraphArr($sentences)
-	{
-		$wordsPer = $this->wordsPerParagraph;
-		$sentenceAvg = $this->wordsPerSentence;
-		$total = count($sentences);
-
-		$paragraphs = array();
-		$pCount = 0;
-		$currCount = 0;
-		$curr = array();
-
-		for($i = 0; $i < $total; $i++)
-		{
-			$s = $sentences[$i];
-			$currCount += count($s);
-			$curr[] = $s;
-			if($currCount >= ($wordsPer - round($sentenceAvg / 2.00)) || $i == $total - 1)
-			{
-				$currCount = 0;
-				$paragraphs[] = $curr;
-				$curr = array();
-				//print_r($paragraphs);
-			}
-			//print_r($paragraphs);
-		}
-
-		return $paragraphs;
-	}
-
-	private function getHTML($count, $loremipsum)
-	{
-		$sentences = $this->getPlain($count, $loremipsum, false);
-		$paragraphs = $this->getParagraphArr($sentences);
-		//print_r($paragraphs);
-
-		$paragraphStr = array();
-		foreach($paragraphs as $p)
-		{
-			$paragraphStr[] = "<p>\n" . $this->paragraphToString($p, true) . '</p>';
-		}
-
-		//add new lines for the sake of clean code
-		return implode("\n", $paragraphStr);
-	}
-
-	private function paragraphToString($paragraph, $htmlCleanCode = false)
-	{
-		$paragraphStr = '';
-		foreach($paragraph as $sentence)
-		{
-			foreach($sentence as $word)
-				$paragraphStr .= $word . ' ';
-
-			if($htmlCleanCode)
-				$paragraphStr .= "\n";
-		}
-		return $paragraphStr;
-	}
-
-	/*
-	* Inserts commas and periods in the given
-	* word array.
-	*/
-	private function punctuate(& $sentence)
+    /**
+     * Inserts commas and periods in the given
+     * word array and capitalises first letter.
+     *
+     * @param array $sentence
+     * @return string
+     */
+    protected function toSentence(array $sentence)
 	{
 		$count = count($sentence);
 		$sentence[$count - 1] = $sentence[$count - 1] . '.';
+        $sentence[0] = ucfirst($sentence[0]);
 
-		if($count < 4)
-			return $sentence;
+		if ($count < 4) {
+			return implode(' ', $sentence);
+        }
 
 		$commas = $this->numberOfCommas($count);
 
-		for($i = 1; $i <= $commas; $i++)
-		{
+		for ($i = 1; $i <= $commas; $i++) {
 			$index = (int) round($i * $count / ($commas + 1));
 
-			if($index < ($count - 1) && $index > 0)
-			{
+			if ($index < ($count - 1) && $index > 0) {
 				$sentence[$index] = $sentence[$index] . ',';
 			}
 		}
+
+        return implode(' ', $sentence);
 	}
 
-	/*
-	* Determines the number of commas for a
-	* sentence of the given length. Average and
-	* standard deviation are determined superficially
-	*/
-	private function numberOfCommas($len)
+    /**
+	 * Determines the number of commas for a
+	 * sentence of the given length. Average and
+	 * standard deviation are determined superficially
+	 *
+     * @param int $len
+     * @return int
+     */
+    public function numberOfCommas($len)
 	{
 		$avg = (float) log($len, 6);
 		$stdDev = (float) $avg / 6.000;
 
-		return (int) round($this->gauss_ms($avg, $stdDev));
+		return (int) round(Statistics::gauss_ms($avg, $stdDev));
 	}
-
-	/*
-	* Returns a number on a gaussian distribution
-	* based on the average word length of an english
-	* sentence.
-	* Statistics Source:
-	*	http://hearle.nahoo.net/Academic/Maths/Sentence.html
-	*	Average: 24.46
-	*	Standard Deviation: 5.08
-	*/
-	private function gaussianSentence()
-	{
-		$avg = (float) 24.460;
-		$stdDev = (float) 5.080;
-
-		return (int) round($this->gauss_ms($avg, $stdDev));
-	}
-
-	/*
-	* The following three functions are used to
-	* compute numbers with a guassian distrobution
-	* Source:
-	* 	http://us.php.net/manual/en/function.rand.php#53784
-	*/
-	private function gauss()
-	{   // N(0,1)
-		// returns random number with normal distribution:
-		//   mean=0
-		//   std dev=1
-
-		// auxilary vars
-		$x=$this->random_0_1();
-		$y=$this->random_0_1();
-
-		// two independent variables with normal distribution N(0,1)
-		$u=sqrt(-2*log($x))*cos(2*pi()*$y);
-		$v=sqrt(-2*log($x))*sin(2*pi()*$y);
-
-		// i will return only one, couse only one needed
-		return $u;
-	}
-
-	private function gauss_ms($m=0.0,$s=1.0)
-	{
-		return $this->gauss()*$s+$m;
-	}
-
-	private function random_0_1()
-	{
-		return (float)rand()/(float)getrandmax();
-	}
-
 }
---
+++ lib/Badcow/LoremIpsum/Statistics.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Badcow\LoremIpsum;
+
+class Statistics
+{
+    /**
+     * Returns random number with normal distribution,
+     * about some $mean with a standard deviation, $stDev.
+     *
+     * @param float $mean
+     * @param float $stDev
+     * @return float
+     */
+    public static function ndRandom($mean = 0.0, $stDev = 1.0)
+    {
+        $x = self::random($mean, $stDev);
+        $y = self::random($mean, $stDev);
+
+        return sqrt(-2 * log($x)) * cos(2 * pi() * $y);
+    }
+
+    /**
+     * Get a normalized random number about some $mean
+     * with a some standard deviation, $stDev.
+     *
+     * @param float $mean
+     * @param float $stDev
+     * @return float
+     */
+    public static function gauss_ms($mean = 0.0, $stDev = 1.0)
+    {
+        return self::ndRandom() * $stDev + $mean;
+    }
+
+    /**
+     * Return a random float on [0,1]
+     *
+     * @return float
+     */
+    public static function random()
+    {
+        return (float) (rand()/getrandmax());
+    }
+}
